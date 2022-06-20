12190193e9e227a5034ec54ba447630167a48007
Fernando Sobrino <sobrino_fernando@yahoo.es>
2022-06-20 22:25:26 +0200

    feat: ya tenemos tres escenas

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -1,6 +1,6 @@
-from importlib import resources
 import pygame as pg
 from arkanoid import ALTO, ANCHO
+from arkanoid.escenas import Portada,Partida,HallOfFame
 
 
 class Arkanoid:
@@ -9,18 +9,19 @@ class Arkanoid:
         pg.init()
         self.display = pg.display.set_mode((ANCHO, ALTO))
         pg.display.set_caption("Keepkanoid")
+        
         icon = pg.image.load("resources/images/icon.png")
         pg.display.set_icon(icon)
+
+        self.escenas = [
+            Portada(self.display),
+            Partida(self.display),
+            HallOfFame(self.display),
+        ]
 
     def jugar(self):
         """Este es el bucle principal"""
         salir = False
         while not salir:
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    salir = True
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_ESCAPE:
-                        salir = True
-                self.display.fill((99, 99, 99))
-                pg.display.flip()
+            for escena in self.escenas:
+                escena.bucle_principal()
